c1d27e456f8749024ddfa6e9f92b5fa3d4bfdd59
Vladyslav <scroynel@gmail.com>
2025-02-23 20:09:28 +0100

    Change keyboard's models and change admin panel

--- keyboard/admin.py
+++ keyboard/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Category, Cart_product, Cart, Order
 
-# Register your models here.
+
+admin.site.register(Cart)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'number', 'image', 'price', 'add_time']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Cart_product)
+class CartProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'cart']
+
+
+@admin.register(Order)
+class OrederAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'create_time', 'is_paid']
--- keyboard/models.py
+++ keyboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -6,12 +7,21 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     image = models.ImageField(upload_to='keyboard_products')
-    price = models.DecimalField(decimal_places=2)
-    status = models.BooleanField()
+    price = models.DecimalField(decimal_places=2, max_digits=9)
+    in_stock = models.BooleanField(default=True)
     number = models.PositiveIntegerField()
-    add_time = models.DateTimeField(auto_now_add=True)
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name='Time')
     edit_time = models.DateTimeField(auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
 
 class Category(models.Model):
@@ -19,15 +29,52 @@ class Category(models.Model):
     slug = models.SlugField()
 
 
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
 class Order(models.Model):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
-    create_time = models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Time')
     is_paid = models.BooleanField(default=False)
 
 
+    def __str__(self):
+        return f'{self.cart.owner.username} - {self.is_paid} - {self.create_time.strftime('%d/%m/%Y - %H:%M')}'
+    
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
 class Cart(models.Model):
-    owner = models.ForeignKey('User', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.owner.username
+    
+
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+
 
 class Cart_product(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.product} - {self.cart}'
+    
+
+    class Meta:
+        verbose_name = 'Cart product'
+        verbose_name_plural = 'Cart products'
