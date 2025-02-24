e087d392c932b876aba21a86e3521da9e13d432a
Vladyslav <scroynel@gmail.com>
2025-02-24 23:58:59 +0100

    Create view for products

--- keyboard/views.py
+++ keyboard/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
+
+class MainView(ListView):
+    model = Product
+    template_name = 'keyboard/main.html'
+    queryset = Product.objects.all()
+
+
+
