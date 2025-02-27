a9f0cd2fd52da041530a9becb1035aa8f554f5ff
Vladyslav <scroynel@gmail.com>
2025-02-27 23:44:05 +0100

    Update main and add views

--- keyboard/urls.py
+++ keyboard/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MainView.as_view(), name='main')
+    path('', views.MainView.as_view(), name='main'),
+    path('keyboards/', views.KeyboardsView.as_view(), name='keyboards'),
+    path('keycaps/', views.KeycapsView.as_view(), name='keycaps'),
+    path('switches/', views.SwitchesView.as_view(), name='switches'),
 ]
--- keyboard/views.py
+++ keyboard/views.py
@@ -8,4 +8,22 @@ class MainView(ListView):
     queryset = Product.objects.all()
 
 
+class KeyboardsView(ListView):
+    model = Product
+    template_name = 'keyboard/keyboards.html'
+    context_object_name = 'keyboards'
+    queryset = Product.objects.filter(category__slug="keyboards")
 
+
+class KeycapsView(ListView):
+    model = Product
+    template_name = 'keyboard/keycaps.html'
+    context_object_name = 'keycaps'
+    queryset = Product.objects.filter(category__slug="keycaps")
+
+
+class SwitchesView(ListView):
+    model = Product
+    template_name = 'keyboard/switches.html'
+    context_object_name = 'switches'
+    queryset = Product.objects.filter(category__slug="switches")
