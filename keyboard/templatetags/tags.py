87925f66a437719c2137886cd86fc5f14b030776
Vladyslav <scroynel@gmail.com>
2025-03-28 23:44:41 +0100

    Add changes to cart

--- keyboard/templatetags/tags.py
+++ keyboard/templatetags/tags.py
@@ -6,14 +6,6 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def cart(context):
+def cart_products(context):
     cart_products = Cart_product.objects.filter(cart=context['request'].user.cart_user.id)
     return cart_products
-
-
-@register.inclusion_tag('keyboard/partials/cart.html', takes_context=True)
-def cart(context):
-    cart_products = Cart_product.objects.filter(cart=context['request'].user.cart_user.id)
-    context['cart_prod'] = cart_products
-    
-    return context
