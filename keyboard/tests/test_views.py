d5b8c1712cf0f922275e46f3fee3af6213aee508
Vladyslav <scroynel@gmail.com>
2025-10-16 22:14:58 +0200

    Adding new tests for views

--- keyboard/tests/test_views.py
+++ keyboard/tests/test_views.py
@@ -1,16 +1,21 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth import get_user_model
 
 from keyboard.models import Category, Product
+from wishlist.models import Wishlist
 
-
+User = get_user_model()
 
 
 class ProductViewsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
+
+        # Create a user
+        cls.user = User.objects.create_user(username='testuser', password='pass')
 
         # Create a category
         cls.category = Category.objects.create(name='Test Category', slug='test-category')
@@ -63,3 +68,54 @@ class ProductViewsTest(TestCase):
         self.assertIn(self.product1, products)
         self.assertIn(self.product2, products)
         self.assertEqual(products.count(), 2)
+
+
+    def test_product_view_context_category(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.context['category_slug'], self.category.slug)
+    
+
+    def test_product_view_invalid_category(self):
+        url = reverse('products', kwargs={'category_slug': 'invalid'})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_product_view_context_user_wishlist_auth(self):
+        self.client.login(username='testuser', password='pass')
+        response = self.client.get(self.url)
+
+        self.assertIn('user_wishlist', response.context)
+
+
+    def test_product_view_context_user_wishlist_anonymous(self):
+        response = self.client.get(self.url)
+
+        self.assertNotIn('user_wishlist', response.context)
+
+
+class MainViewsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+
+        cls.url = reverse('main')
+
+    def test_main_view_returns_200(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_main_view_template_used(self):
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed(response, 'keyboard/main.html')
+
+
+    def test_main_view_context_products(self):
+        response = self.client.get(self.url)
+
+        self.assertIn('products', response.context)
