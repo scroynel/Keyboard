7524b57dddd6b2e553a7e2d512e9c6b39ec3fca2
Vladyslav <scroynel@gmail.com>
2025-10-25 21:40:40 +0200

    Adding test for all views classes and create common class with setup

--- keyboard/tests/test_views.py
+++ keyboard/tests/test_views.py
@@ -12,7 +12,7 @@ from keyboard.views import ProductDetailView
 User = get_user_model()
 
 
-class ProductViewsTest(TestCase):
+class BaseTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
@@ -49,6 +49,20 @@ class ProductViewsTest(TestCase):
             image = image
         )
 
+        # Create a user
+        cls.user1 = User.objects.create_user(username='testuser1', password='pass')
+        cls.user2 = User.objects.create_user(username='testuser2', password='pass')
+        cls.user3 = User.objects.create_user(username='testuser3', password='pass')
+
+        # Create comments 
+        cls.comment1 = ProductComment.objects.create(description='Test description1', product=cls.product1, owner=cls.user1, rating=4)
+        cls.comment2 = ProductComment.objects.create(description='Test description2', product=cls.product1, owner=cls.user2, rating=5)
+
+
+class ProductViewsTest(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
         cls.url = reverse('products', kwargs={'category_slug': cls.category.slug})
 
 
@@ -87,7 +101,7 @@ class ProductViewsTest(TestCase):
 
 
     def test_product_view_context_user_wishlist_auth(self):
-        self.client.login(username='testuser', password='pass')
+        self.client.login(username='testuser1', password='pass')
         response = self.client.get(self.url)
 
         self.assertIn('user_wishlist', response.context)
@@ -99,11 +113,10 @@ class ProductViewsTest(TestCase):
         self.assertNotIn('user_wishlist', response.context)
 
 
-class MainViewsTest(TestCase):
+class MainViewsTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.client = Client()
-
+        super().setUpTestData()
         cls.url = reverse('main')
 
     def test_main_view_returns_200(self):
@@ -124,38 +137,10 @@ class MainViewsTest(TestCase):
         self.assertIn('products', response.context)
 
 
-class ProductDetailViewTest(TestCase):
+class ProductDetailViewTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.client = Client()
-
-        cls.category = Category.objects.create(name='Test Category', slug='test-category')
-
-        image = SimpleUploadedFile(
-            name='test_image.jpg',
-            content=b'\x47\x49\x46\x38\x39\x61',  # minimal GIF header bytes
-            content_type='image/gif'
-        )
-
-        # Create products
-        cls.product1 = Product.objects.create(
-            name = 'Test Product',
-            slug = 'test-product',
-            description = 'Description test product',
-            price = 100.00,
-            number = 567845,
-            category = cls.category,
-            image = image
-        )
-
-        # Create users
-        cls.user1 = User.objects.create(username='testuser', password='pass')
-        cls.user2 = User.objects.create(username='testuser1', password='pass')
-
-        # Create comments 
-        cls.comment1 = ProductComment.objects.create(product=cls.product1, owner=cls.user1, rating=5)
-        cls.comment2 = ProductComment.objects.create(product=cls.product1, owner=cls.user2, rating=4)
-
+        super().setUpTestData()
         cls.url = reverse('products_detail', kwargs={'category_slug': cls.category.slug, 'product_slug': cls.product1.slug})
 
 
@@ -184,34 +169,10 @@ class ProductDetailViewTest(TestCase):
         self.assertIn(self.comment2, comments)
 
 
-class AjaxCommentAddViewTest(TestCase):
+class AjaxCommentAddViewTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.client = Client()
-
-        # Create a User
-        cls.user1 = User.objects.create_user(username='testuser', password='pass')
-
-        # Create a category
-        cls.category = Category.objects.create(name='Test Category', slug='test-category')
-
-        image = SimpleUploadedFile(
-            name='test_image.jpg',
-            content=b'\x47\x49\x46\x38\x39\x61',  # minimal GIF header bytes
-            content_type='image/gif'
-        )
-
-        # Create a product
-        cls.product1 = Product.objects.create(
-            name = 'Test Product',
-            slug = 'test-product',
-            description = 'Description test product',
-            price = 100.00,
-            number = 567845,
-            category = cls.category,
-            image = image
-        )
-
+        super().setUpTestData()
         cls.url = reverse('comment_add', kwargs={'category_slug': cls.category.slug, 'product_slug': cls.product1.slug})
 
     
@@ -220,7 +181,7 @@ class AjaxCommentAddViewTest(TestCase):
         data = {
             'description': 'Test description1',
             'rating': 3,
-            'owner': self.user1,
+            'owner': self.user3,
             'product': self.product1
         }
         response = self.client.post(self.url, data)
@@ -233,7 +194,7 @@ class AjaxCommentAddViewTest(TestCase):
         data = {
             'description': 'Test description2',
             'rating': 4,
-            'owner': self.user1,
+            'owner': self.user3,
             'product': self.product1
         }
         headers = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
@@ -244,20 +205,13 @@ class AjaxCommentAddViewTest(TestCase):
         self.assertIn('avg_rating', response.json())
         self.assertIn('comments_count', response.json())
         self.assertIn('comment_list', response.json())
-        self.assertEqual(ProductComment.objects.count(), 1)
+        self.assertEqual(ProductComment.objects.count(), 3)
 
     
-    def test_ajax_comment_add_view_one_comment(self):
-        ProductComment.objects.create(
-            description = 'Test description1',
-            rating = 3,
-            owner = self.user1,
-            product = self.product1
-        )
-
-        self.client.login(username='testuser', password='pass')
+    def test_ajax_comment_add_view_second_comment(self):
+        self.client.login(username='testuser1', password='pass')
         data = {
-            'description': 'Test description2',
+            'description': 'Test description1',
             'rating': 4,
             'owner': self.user1,
             'product': self.product1
@@ -267,7 +221,7 @@ class AjaxCommentAddViewTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertIn('status_error', response.json())
-        self.assertEqual(ProductComment.objects.count(), 1)
+        self.assertEqual(ProductComment.objects.count(), 2)
 
     
     def test_ajax_comment_add_view_no_comment(self):
@@ -275,11 +229,11 @@ class AjaxCommentAddViewTest(TestCase):
         data = {
             'description': '',
             'rating': 6,
-            'owner': self.user1,
+            'owner': self.user3,
             'product': self.product1
         }
         headers = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
         response = self.client.post(self.url, data, **headers)
 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(ProductComment.objects.count(), 0)
+        self.assertEqual(ProductComment.objects.count(), 2)
